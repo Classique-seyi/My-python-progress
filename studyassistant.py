c561c3639b360308ff9055c3f049738da1563d64
Classique_seyi <adelekeprecious18@gmail.com>
2026-09-22 16:00:03 +0100

    so far

--- studyassistant.py
+++ studyassistant.py
@@ -51,11 +51,21 @@ class StudyAssistant:
                 json.dump(data, file)
     
     def load_notes(self):
-        with open("notes.json", "r") as file:
-            data = json.load(file) 
-            for note in data:
-                new_note = Note(note["Title"], note["Content"], note["Subject"])
-                self.notes.append(new_note)
+        try:
+            with open("notes.json", "r") as file:
+                data = json.load(file) 
+                for note in data:
+                    new_note = Note(note["Title"], note["Content"], note["Subject"])
+                    self.notes.append(new_note)
+        except FileNotFoundError:
+            pass
+
+    def edit_notes(self, note_number, new_title, new_content, new_subject):
+        index = note_number - 1
+        note = self.notes[index]
+        note.title = new_title
+        note.content = new_content
+        note.subject = new_subject
 
     def add_task(self,task):
         self.tasks.append(task)
@@ -90,6 +100,13 @@ class StudyAssistant:
                     self.tasks.append(new_task)
         except FileNotFoundError:
             pass
+
+    def edit_task(self, task_number, new_description):
+        index = task_number - 1
+        task = self.tasks[index]
+        task.description = new_description
+       
+        
 
     
 
@@ -126,12 +143,14 @@ def main():
 6. Complete task
 7. Exit
 8. Delete task
+9. Edit task
+10. Edit note
 ''')
         choice = input("Choose an option: ")
         if choice == "1":
-            title = input("Enter note title: ").strip()
-            content = input("Enter note content: ").strip()
-            subject = input("Enter note subject: ").strip()
+            title = input("Enter note title: ").strip().title()
+            content = input("Enter note content: ").strip().capitalize()
+            subject = input("Enter note subject: ").strip().capitalize()
 
             if not title or not content:
                 print("Note title and content cannot be empty.")
@@ -180,10 +199,47 @@ def main():
             break
 
         elif choice == "8":
-            task_number = int
+            try:
+                task_number = int(input("Enter task number to delete: "))
+                if task_number <= 0 or task_number > len(assistant.tasks):
+                    print("Task number not found.")
+                else:
+                    assistant.delete_task(task_number)
+                    assistant.save_tasks()
+            except ValueError:
+                print("Please enter a valid task number.")
+
+        elif choice == "9":
+            try:
+                task_number = int(input("Enter task number to edit: "))
+                new_description = input("Enter your new description: ").strip()
+                if task_number <= 0 or task_number > len(assistant.tasks):
+                    print("Task number not found.")
+                else:
+                    assistant.edit_task(task_number, new_description)
+                    assistant.save_tasks()
+            except ValueError:
+                print("Please enter a valid task number")
+
+        elif choice == "10":
+            try:
+                note_number = int(input("Enter note number to edit: "))
+                new_title = input("Enter your new title: ").strip().title()
+                new_content = input("Enter your new content: ").strip().capitalize()
+                new_subject = input("Enter your new subject: ").strip().capitalize()
+                if note_number <= 0 or note_number > len(assistant.notes):
+                    print("Note number not found.")
+                elif not new_title or not new_content:
+                    print("New title or new content not entered")
+                else:
+                    assistant.edit_notes(note_number, new_title, new_content, new_subject)
+                    assistant.save_notes()
+            except ValueError:
+                print("Please enter a valid note number.")
+
 
         else:
-            print("Invalid option. Please choose a number from 1 to 7.")
+            print("Invalid option. Please choose a number from 1 to 10.")
 if __name__ == "__main__":
     main()
     
@@ -221,13 +277,13 @@ task3 = Task(
 
 assistant = StudyAssistant()
 
-# assistant.add_note(note1)
-# assistant.add_note(note2)
-# assistant.add_note(note3)
+assistant.add_note(note1)
+assistant.add_note(note2)
+assistant.add_note(note3)
 
-# assistant.add_task(task1)
-# assistant.add_task(task2)
-# assistant.add_task(task3)
+assistant.add_task(task1)
+assistant.add_task(task2)
+assistant.add_task(task3)
 
 # task1.mark_complete()
 
